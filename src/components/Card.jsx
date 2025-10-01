b055ec817a6237d447f9c9aac1b34b4de90ed599
DiegoFatVega <fatvegadiego@gmail.com>
2025-10-01 22:18:20 +0200

    Refactor Card component to display language items dynamically from data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,14 @@
+import languages from "../data/languages"
+
 export default function Card() {
     return (
         <div className="Card">
-            <h2>title</h2>
-            <h3>description</h3>
+            {languages.map(item => (
+                <div className="item" key={item.id}>
+                    <h2>{item.title}</h2>
+                    <h3>{item.description}</h3>
+                </div>
+            ))}
         </div>
     )
 }
