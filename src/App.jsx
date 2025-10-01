25c83ef9adcd1ff201922ab29367ab48879690f8
DiegoFatVega <fatvegadiego@gmail.com>
2025-10-01 21:48:58 +0200

    Add AccordionButtons component and update App to include it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import AccordionButtons from './components/AccordionButtons'
 
 function App() {
 
   return (
     <>
-
+      <h1>Learn Web development</h1>
+      <AccordionButtons></AccordionButtons>
     </>
   )
 }
---
+++ src/components/AccordionButtons.jsx
@@ -0,0 +1,14 @@
+export default function AccordionButtons() {
+    return (
+        <div className="items">
+            <button>HTML</button>
+            <button>CSS</button>
+            <button>JavaScript</button>
+            <button>Node.js</button>
+            <button>Express</button>
+            <button>ReactJs</button>
+        </div>
+
+    )
+
+}
